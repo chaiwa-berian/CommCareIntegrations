cefbd011d058c39f09aa85b3a803817359911b62
Chaiwa Berian <chaiwa.berian@gmail.com>
2018-01-04 14:18:11 +0200

    Add stub code to create new waterpoint

---
+++ createNewWaterPoint.js
@@ -0,0 +1,17 @@
+// The code to create a new Waterpoint into DHIS2 Database
+createTEI(
+    fields(
+      field("trackedEntity","nEenWmSyUEp"),
+      field("orgUnit", "DiszpKrYNg8"),
+      field("attributes", function(state){
+        return [{
+          "attribute": "w75KJ2mc4zz",
+          "value": dataValue("form.test")(state)
+        },
+        {
+          "attribute": "ryru7yeHjsjs",
+          "value:": dataValue("form.username")(state)
+        }]
+      })
+      )
+    )

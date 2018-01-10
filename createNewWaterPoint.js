a7b4f30afe2748aa5116eebda09721eefc72b30f
Chaiwa Berian <chaiwa.berian@gmail.com>
2018-01-10 19:08:52 +0200

    Working version of createTEI

--- createNewWaterPoint.js
+++ createNewWaterPoint.js
@@ -1,17 +1,26 @@
 // The code to create a new Waterpoint into DHIS2 Database
-createTEI(
-    fields(
-      field("trackedEntity","nEenWmSyUEp"),
-      field("orgUnit", "DiszpKrYNg8"),
-      field("attributes", function(state){
-        return [{
-          "attribute": "w75KJ2mc4zz",
-          "value": dataValue("form.test")(state)
-        },
-        {
-          "attribute": "ryru7yeHjsjs",
-          "value:": dataValue("form.username")(state)
-        }]
-      })
-      )
-    )
+createTEI( {
+  "trackedEntity": "VENu24Hqrg5",
+  "orgUnit": "qCaJ3CY1rgw",
+  "attributes": [
+    {
+    "attribute": "Exn9iU61K5S",
+    "value": dataValue("form.name")(state)
+    },
+    {
+      "attribute": "TFk7uLjDrB7",
+      "value": dataValue("form.gps_coordinates")(state)
+    },
+    {
+      "attribute": "hA9s4T7EC7x",
+      "value": dataValue("form.lattitude")(state)
+    },
+    {
+      "attribute": "HgQVDJTn5p6",
+      "value": dataValue("form.longitude")(state)
+    },
+    {
+      "attribute": "dyuCklbsDXs",
+      "value": dataValue("form.waterpoint_id")(state)
+    }]
+}   )

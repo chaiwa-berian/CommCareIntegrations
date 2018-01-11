46dd31083e4969a22e85c4d3503e75dbecef0e58
Chaiwa Berian <chaiwa.berian@gmail.com>
2018-01-11 14:29:50 +0200

    Add example config for submitting a form to CommCare via OpenFn

--- Configurations.js
+++ Configurations.js
@@ -41,3 +41,34 @@ createTEI( {
 /************************************* 
  ****B. DHIS2 to CommCare Job Setup****
  ************************************/
+
+ //1. Credential Settings
+ Label: CommCare-Mawa WASH MEAL App
+ Host URL: https://www.commcarehq.org
+ Application Name(Project Domain): mawa-wash
+ Application UUID: 3ebb1ab0124313650b86d7668645f556
+ Username: Commcare Username
+ Password: CommCare Password
+
+ //2. Trigger Settings
+ Label: Submit New Waterpoint to CommCare
+ Trigger Type: Message Filter: Job runs when matching message arrives
+ Filter Criteria: {"formid":"3ebb1ab0124313650b86d7668645f556"}
+
+ //3. Job Code Snippet to Create a new ntity Instance(Equivalent to CommCare's Case)
+ submit(
+    fields(
+      field("@", function(state) {
+        return {
+          "xmlns": "http://openrosa.org/formdesigner/C3478C08-ABB3-47FC-ADF1-A7788E28D565"
+        };
+      }),
+      field("name", "Coliform Waterpoint"),
+      field("waterpoint_id", "102772815"),
+      field("gps_coordinates", "-13.6426553 32.6390061 0.0 20.0"),
+      field("lattitude", "-13.6426553"),
+      field("longitude", "32.6390061")
+      )
+  )
+
+

10a48312f4350d9a65b9c7e2f6beb57a5065e5c9
Chaiwa Berian <chaiwa.berian@gmail.com>
2018-01-11 12:28:51 +0200

    Add stub code for submitting form to CommCare

---
+++ submitNewWaterPointToCommCare.js
@@ -0,0 +1,14 @@
+submit(
+    fields(
+      field("@", function(state) {
+        return {
+          "xmlns": "http://openrosa.org/formdesigner/C3478C08-ABB3-47FC-ADF1-A7788E28D565"
+        };
+      }),
+      field("name", "Coliform Waterpoint"),
+      field("waterpoint_id", "102772815"),
+      field("gps_coordinates", "-13.6426553 32.6390061 0.0 20.0"),
+      field("lattitude", "-13.6426553"),
+      field("longitude", "32.6390061")
+      )
+  )

df5be024b6eb246d3b0139ec64ef05dbe858ec17
Chaiwa Berian <chaiwa.berian@gmail.com>
2018-01-12 10:58:20 +0200

    Add metadata tag to the payload

--- submitNewWaterPointToCommCare.js
+++ submitNewWaterPointToCommCare.js
@@ -1,4 +1,19 @@
 submit(
+    field("metadata", function(state){
+      return {
+        "username": "test",
+        "userID": "9231768348d64a5aa8e2df401c6ca7fd",
+        "timeStart": "2018-01-10T17:22:28.094000Z",
+        "timeEnd": "2018-01-10T17:23:20.373000Z",
+        "location": null,
+        "geo_point": null,
+        "deviceID": "357950075597380",
+        "commcare_version": "2.40.1",
+        "app_build_version": 34,
+        "appVersion": "CommCare Android, version \"2.40.1\"(444156). App v34. CommCare Version 2.40. Build 444156, built on: 2017-11-20"
+
+      }
+    }),
     fields(
       field("@", function(state) {
         return {
@@ -10,7 +25,7 @@ submit(
       field("gps_coordinates", "-13.6426553 32.6390061 0.0 20.0"),
       field("lattitude", "-13.6426553"),
       field("longitude", "32.6390061"),
-      field("metadata", function(state) {
+      field("meta", function(state) {
           return {
             "username": "test",
             "userID": "9231768348d64a5aa8e2df401c6ca7fd",
